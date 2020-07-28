ca8eec9f8b23303bc63a4b735d1e0267cb1ef278
yqz-olympus <64834532+yqz-olympus@users.noreply.github.com>
2020-07-28 14:40:36 +0800

    refactor LinkedList

--- kronus/src/main/java/com/olympus/framework/kronus/linkedlist/LinkedList.java
+++ kronus/src/main/java/com/olympus/framework/kronus/linkedlist/LinkedList.java
@@ -4,10 +4,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class LinkedList<E extends Comparable<E>> {
-    private static final Logger LOGGER = LogManager.getLogger(LinkedList.class);
 
-    private static class Node<E> {
-        private final E e;
+    private class Node<E> {
+        private E e;
         private Node<E> next;
         private Node<E> prev;
 
@@ -18,7 +17,7 @@ public class LinkedList<E extends Comparable<E>> {
         }
 
         public Node(E e) {
-            this(e, null, null);
+            this.e = e;
         }
     }
 
@@ -40,82 +39,19 @@ public class LinkedList<E extends Comparable<E>> {
         return size == 0;
     }
 
-    /***
-     * insert elements from the end for the linked list
-     * @param e the element to be inserted
-     */
-    public void insert(E e) {
+    public void insertFromTheHead(E e) {
 
-        Node<E> node = new Node<E>(e);
-        if (tail == null) {
-            head = tail = node;
-        } else {
-            tail.next = node;
-            node.prev = tail;
-            tail = node;
-        }
-
-        size++;
     }
 
-    public E pop() {
-        if (isEmpty()) {
-            throw new IllegalArgumentException("cannot stack from an empty queue");
-        }
+    public void insertFromTheTail(E e) {
 
-        Node<E> temp = tail;
-        tail = tail.prev;
-        temp.prev = null;
-
-        if (tail == null) {
-            head = null;
-        } else {
-            tail.next = null;
-        }
-
-        size--;
-
-        return temp.e;
     }
 
-    /***
-     * remove from the beginning of the link list
-     * @return returns the deleted element
-     */
-    public E dequeue() {
-        if (isEmpty()) {
-            throw new IllegalArgumentException("cannot dequeue from an empty queue");
-        }
-
-        Node<E> temp = head;
-        head = head.next;
-        temp.next = null;
-
-        if (head == null) {
-            tail = null;
-        } else {
-            head.prev = null;
-        }
-
-        size--;
-
-        return temp.e;
+    public E deleteFromTheTail() {
+        return null;
     }
 
-    public E getHead() {
-        if (isEmpty()) {
-            throw new IllegalArgumentException("cannot dequeue from an empty queue");
-        }
-
-        return head.e;
-    }
-
-    public E getTail(){
-        if (isEmpty()) {
-            throw new IllegalArgumentException("cannot stack from an empty queue");
-        }
-
-        return tail.e;
+    public E deleteFromTheHead() {
+        return null;
     }
 }
-
