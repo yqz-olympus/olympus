40ba2286570afacf561a7f80ffaf2251e23ebb1b
yqz-olympus <64834532+yqz-olympus@users.noreply.github.com>
2020-08-09 14:36:06 +0800

    BinarySearchTree add insert order function

--- kronus/src/main/java/com/olympus/framework/kronus/tree/BinarySearchTree.java
+++ kronus/src/main/java/com/olympus/framework/kronus/tree/BinarySearchTree.java
@@ -1,5 +1,8 @@
 package com.olympus.framework.kronus.tree;
 
+import com.olympus.framework.kronus.queue.Queue;
+import com.olympus.framework.kronus.queue.impl.LinkedListQueue;
+
 public class BinarySearchTree<E extends Comparable<E>> {
 
     private class Node<E> {
@@ -29,5 +32,95 @@ public class BinarySearchTree<E extends Comparable<E>> {
     public int size() {
         return size;
     }
+
+    public void insert(E e) {
+        root = insert(root, e);
+    }
+
+    private Node<E> insert(Node<E> node, E e) {
+
+        if (node == null) {
+            size++;
+            return new Node<E>(e);
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = insert(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = insert(node.right, e);
+        }
+
+        return node;
+    }
+
+    public boolean contains(E e) {
+
+        return contains(root, e);
+    }
+
+    private boolean contains(Node<E> node, E e) {
+
+        if (node == null) {
+            return false;
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            return contains(node.right, e);
+        } else {
+            return true;
+        }
+
+    }
+
+    public Queue<E> postOrder() {
+        Queue<E> queue = new LinkedListQueue<>();
+        postOrder(root, queue);
+        return queue;
+    }
+
+    private void postOrder(Node<E> node, Queue<E> queue) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left, queue);
+        postOrder(node.right, queue);
+        queue.enqueue(node.e);
+    }
+
+    public Queue<E> inOrder() {
+        Queue<E> queue = new LinkedListQueue<>();
+        inOrder(root, queue);
+        return queue;
+    }
+
+    private void inOrder(Node<E> node, Queue<E> queue) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left, queue);
+        queue.enqueue(node.e);
+        inOrder(node.right, queue);
+    }
+
+    public Queue<E> preOrder() {
+        Queue<E> queue = new LinkedListQueue<E>();
+        preOrder(root, queue);
+        return queue;
+    }
+
+    private void preOrder(Node<E> node, Queue<E> queue) {
+        if (node == null) {
+            return;
+        }
+
+        queue.enqueue(node.e);
+        preOrder(node.left, queue);
+        preOrder(node.right, queue);
+    }
+
 
 }
---
+++ kronus/src/test/java/com/olympus/framework/kronus/tree/BinarySearchTreeTest.java
@@ -0,0 +1,40 @@
+package com.olympus.framework.kronus.tree;
+
+import com.olympus.framework.kronus.queue.Queue;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearchTreeTest {
+
+    @Test
+    void bst() throws Exception {
+        BinarySearchTree<Integer> bst = new BinarySearchTree<>();
+        int[] array = new int[]{5, 3, 6, 8, 4, 2};
+        assertEquals(0, bst.size());
+        assertTrue(bst.isEmpty());
+
+        for (int j : array) {
+            bst.insert(j);
+        }
+        assertEquals(6, bst.size());
+        assertFalse(bst.isEmpty());
+
+        Queue<Integer> pre = bst.preOrder();
+        assertEquals(5, pre.dequeue());
+        assertEquals(3, pre.dequeue());
+        assertEquals(2, pre.dequeue());
+        assertEquals(4, pre.dequeue());
+        assertEquals(6, pre.dequeue());
+        assertEquals(8, pre.dequeue());
+
+
+        Queue<Integer> in = bst.inOrder();
+        assertEquals(2, in.dequeue());
+        assertEquals(3, in.dequeue());
+        assertEquals(4, in.dequeue());
+        assertEquals(5, in.dequeue());
+        assertEquals(6, in.dequeue());
+        assertEquals(8, in.dequeue());
+    }
+}
